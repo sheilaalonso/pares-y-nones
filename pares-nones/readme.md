248aed0ff2f72e50a2a933abdb5157d78bc7c4cb
sheilaalonso <sheilachei0289@gmail.com>
2017-03-09 10:50:17 +0100

    código de inicio
    Subo el código muy básico que ya mejorare.

---
+++ pares-nones/readme.md
@@ -0,0 +1,13 @@
+****Juego de pares y nones****
+Escenario: pantalla de juego pares y nones
+En principio el juego lo hacemnos con entradas y salidas por consola.
+2 jugadores
+El jugador 1 es nones
+El jugador 2 es pares
+Se le pide al jugador 1 que elija del 1 al 5.
+Se le pide al jugadro 2 que elija del 1 al 5.
+Se suman y se hace modulo de la suma
+Si sale impares gana el jugador 1.
+Si sale pares gana el jugador 2.
+
+
---
+++ pares-nones/script.js
@@ -0,0 +1,17 @@
+
+
+
+document.write('vamos a jugar a pares y nones. El jugador 1 siempre elige nones y el jugador 2 siempre elige pares <br>');
+var tiradaJugador = prompt('Jugador 1 dinos un numero del 1 al 5');
+document.write('El jugador 1 ha sacado = '+ tiradaJugador + '<br>');
+var tiradaJugador2 = prompt('Jugador2 dinos un numero del 1 al 5');
+document.write('El jugador 2 ha sacado = '+ tiradaJugador2);
+tiradaJugador = Number(tiradaJugador);
+tiradaJugador2 = Number(tiradaJugador2);
+var suma = tiradaJugador + tiradaJugador2;
+document.write('La suma de jugadas es = ' + suma+ '<br>');
+if(suma % 2 === 0){
+   document.write('El jugador 2 ha ganado <br>');
+}else{
+  document.write('El jugador 1 ha ganado <br>'); 
+}
